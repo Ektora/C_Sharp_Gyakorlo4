d1de3e500870151ea25822d98f7e6f431d603dce
Ektora <zsoltlucski@gmail.com>
2022-08-29 21:06:28 +0200

    Add project files.

---
+++ Gyakorlo4/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlo4
+{
+    class Rectangle
+    {
+        public double width = 1.0, height = 1.0;
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(double height, double width)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double getArea()
+        {
+            return this.width * this.height;
+        }
+
+        public double getPerimeter()
+        {
+            return 2 * (this.width + this.height);
+        }
+    }
+}
---
+++ Gyakorlo4/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Gyakorlo4 { 
+public class Gyakorlo4
+{
+    public static void Main(string[] args)
+    {
+            Rectangle r1 = new Rectangle(40.0, 4.0);
+            Rectangle r2 = new Rectangle(35.9, 3.5);
+            Console.WriteLine($"{r1.height} {r1.width} {r1.getArea()} {r1.getPerimeter()}");
+            Console.WriteLine($"{r2.height} {r2.width} {r2.getArea()} {r2.getPerimeter()}");
+        }
+}
+}
